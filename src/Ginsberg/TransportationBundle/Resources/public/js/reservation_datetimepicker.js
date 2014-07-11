21ed654777d4fc2c01da6989659030731c4c1583
ackerman <ericaack@m-d25hd0szdhjt.local>
2014-07-10 20:47:10 -0400

    Using a function to populate the allowTimes option

--- src/Ginsberg/TransportationBundle/Resources/public/js/reservation_datetimepicker.js
+++ src/Ginsberg/TransportationBundle/Resources/public/js/reservation_datetimepicker.js
@@ -1,10 +1,23 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Enable the jQuery plugin datetimepicker on various fields in the
+ * Reservation Add and Edit forms.
+ * 
+ * For details on using the plugin, see:
+ * http://xdsoft.net/jqplugins/datetimepicker/. The version used here is
+ * slightly modified from the original to provide AM/PM formatting in the
+ * Timepicker.
  */
 jQuery(document).ready(function($) {
-  var allowed_times = ['08:00', '08:15', '08:30', '08:45', '09:00', '09:15', '09:30', '09:45', '10:00', '10:15', '10:30', '10:45', '11:00', '11:15', '11:30', '11:45', '12:00', '12:15', '12:30', '12:45', '13:00', '13:15', '13:30', '13:45', '14:00', '14:15', '14:30', '14:45', '15:00', '15:15', '15:30', '15:45', '16:00', '16:15', '16:30', '16:45', '17:00', '17:15', '17:30', '17:45', '18:00', '18:15', '18:30', '18:45', '19:00', '19:15', '19:30', '19:45', '20:00'];
+  var allowTimesArray = [];
+  for (var i = 8, j = 0; i < 20; i++) {
+    for (j = 0; j < 60; j += 15) {
+      h = (i < 10 ? '0' : '') + i;
+      m = (j < 10 ? '0' : '') + j;
+      var myTime = h + ':' + m;
+      allowTimesArray.push(myTime);
+    }
+  }
+  allowTimesArray.push('20:00');
   
   $('#ginsberg_transportationbundle_reservation_start').datetimepicker({
     onGenerate:function( ct ){
@@ -15,7 +28,7 @@ jQuery(document).ready(function($) {
     inline: false,
     mindate: 0,
     weekends: ['06.01.2014', '07.01.2014','08.01.2014','09.01.2014','10.01.2014','11.01.2014','12.01.2014','01.01.2015','02.01.2015','03.01.2015','04.01.2015','05.01.2015','06.01.2015'],
-    allowTimes: allowed_times,
+    allowTimes: allowTimesArray,
     yearStart: '2014',
     yearEnd: '2016',
     roundTime: 'round',
@@ -32,7 +45,7 @@ jQuery(document).ready(function($) {
     inline: false,
     mindate: 0,
     weekends: ['06.01.2014', '07.01.2014','08.01.2014','09.01.2014','10.01.2014','11.01.2014','12.01.2014','01.01.2015','02.01.2015','03.01.2015','04.01.2015','05.01.2015','06.01.2015'],
-    allowTimes: allowed_times,
+    allowTimes: allowTimesArray,
     yearStart: '2014',
     yearEnd: '2016',
     roundTime: 'round',
@@ -49,7 +62,7 @@ jQuery(document).ready(function($) {
     inline: false,
     mindate: 0,
     weekends: ['06.01.2014', '07.01.2014','08.01.2014','09.01.2014','10.01.2014','11.01.2014','12.01.2014','01.01.2015','02.01.2015','03.01.2015','04.01.2015','05.01.2015','06.01.2015'],
-    allowTimes: allowed_times,
+    allowTimes: allowTimesArray,
     yearStart: '2014',
     yearEnd: '2016',
     roundTime: 'round',
@@ -66,7 +79,7 @@ jQuery(document).ready(function($) {
     inline: false,
     mindate: 0,
     weekends: ['06.01.2014', '07.01.2014','08.01.2014','09.01.2014','10.01.2014','11.01.2014','12.01.2014','01.01.2015','02.01.2015','03.01.2015','04.01.2015','05.01.2015','06.01.2015'],
-    allowTimes: allowed_times,
+    allowTimes: allowTimesArray,
     yearStart: '2014',
     yearEnd: '2016',
     roundTime: 'round',
